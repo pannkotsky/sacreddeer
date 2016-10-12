890ae4bb4a4e79d71b42cf8e9eeb589007fe62d3
pannkotsky <kovvalole@gmail.com>
2016-10-13 01:36:26 +0300

    Add langs, logging, tests

--- sacreddeer/bot.py
+++ sacreddeer/bot.py
@@ -1,81 +1,125 @@
+import logging
 import os
 import random
 import time
 
 import slackclient
 
-BOT_NAME = 'sacred_deer'
-BOT_ID = None # 'U2M6BHP4Y'
-TOKEN = os.environ.get('TOKEN')
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.INFO)
 
 
-slack_client = slackclient.SlackClient(TOKEN)
+class Bot(object):
+    def __init__(self, name, token):
+        self.name = name
+        self.client = slackclient.SlackClient(token)
+        self.bot_id = self._get_bot_id()
 
-def get_bot_id():
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-            if 'name' in user and user.get('name') == BOT_NAME:
-                return user.get('id')
-    else:
-        raise LookupError("Could not find bot user with the name " + BOT_NAME)
+    def _get_bot_id(self):
+        api_call = self.client.api_call("users.list")
+        if api_call.get('ok'):
+            # retrieve all users so we can find our bot
+            users = api_call.get('members')
+            for user in users:
+                if 'name' in user and user.get('name') == self.name:
+                    return user.get('id')
+        else:
+            raise LookupError("Could not find bot user with the name " +
+                              self.name)
 
+    def handle_command(self, cmd, chan):
+        """
+            Receives commands directed at the bot and determines if they
+            are valid commands. If so, then acts on the commands. If not,
+            returns back what it needs for clarification.
+        """
+        responses = {
+            'en': [
+                "Yes",
+                "No",
+                "It doesn't matter",
+                "Chill, bro",
+                "Ha-ha, very funny",
+                "Yes, but shouldn't",
+                "Never",
+                "100%",
+                "1 of 100",
+                "Try again"
+            ],
+            'ru': [
+                "Да",
+                "Нет",
+                "Это не важно",
+                "Спок, бро",
+                "Толсто",
+                "Да, хотя зря",
+                "Никогда",
+                "100%",
+                "1 из 100",
+                "Еще разок"
+            ],
+            'ua': [
+                "Так",
+                "Ні",
+                "Немає значення",
+                "Не сци, козаче",
+                "Товсто",
+                "Так, хоча даремно",
+                "Ніколи",
+                "100%",
+                "1 із 100",
+                "Спробуй ще"
+            ],
+            'de': [
+                "Ja",
+                "Nein",
+                "Das ist nicht jebachtung",
+                "Uspokojten, Bruder",
+                "Tolstische",
+                "Ja, aber Dolbojobist",
+                "Nie",
+                "100%",
+                "1 von 100",
+                "Poprobiren es noch einmal"
+            ]
+        }
+        lang = cmd[:2] if len(cmd) >= 2 else 'ru'
+        responses_list = responses.get(lang) or responses['ru']
+        response = random.choice(responses_list)
+        self.client.api_call("chat.postMessage", channel=chan,
+                             text=response, as_user=True)
+        logging.info('Command: "%s", response: "%s"', cmd, response)
 
-def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    responses = [
-        "Yes",
-        "Niet",
-        "It doesn't matter",
-        "Chill, bro",
-        "Tolsto",
-        "Yes, but zrya",
-        "Nein! Nein! Nein!!!",
-        "100%",
-        "1 of 100",
-        "Try again"
-    ]
-    response = random.choice(responses)
-    slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
-    print(time.strftime('%d-%m-%Y %H:%M.%S')+ ' ' + command + ' --- ' + response)
+    def parse_slack_output(self, slack_rtm_output):
+        """
+            The Slack Real Time Messaging API is an events firehose.
+            this parsing function returns None unless a message is
+            directed at the Bot, based on its ID.
+        """
+        at_bot = "<@" + self.bot_id + ">"
 
+        output_list = slack_rtm_output
+        if output_list and len(output_list) > 0:
+            for output in output_list:
+                if output and 'text' in output and at_bot in output['text']:
+                    # return text after the @ mention, whitespace removed
+                    return output['text'].split(at_bot)[1].strip().lower(), \
+                           output['channel']
+        return None, None
 
-def parse_slack_output(slack_rtm_output):
-    """
-        The Slack Real Time Messaging API is an events firehose.
-        this parsing function returns None unless a message is
-        directed at the Bot, based on its ID.
-    """
-    global BOT_ID
-    if not BOT_ID:
-        BOT_ID = get_bot_id()
-    at_bot = "<@" + BOT_ID + ">"
-    
-    output_list = slack_rtm_output
-    if output_list and len(output_list) > 0:
-        for output in output_list:
-            if output and 'text' in output and at_bot in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(at_bot)[1].strip().lower(), \
-                       output['channel']
-    return None, None
+    def run(self, read_socket_delay=1):
+        if self.client.rtm_connect():
+            logging.info("Sacred Deer connected and running!")
+            while True:
+                command, channel = self.parse_slack_output(
+                    self.client.rtm_read())
+                if command and channel:
+                    self.handle_command(command, channel)
+                time.sleep(read_socket_delay)
+        else:
+            logging.error("Connection failed. Invalid Slack token or bot ID?")
 
 
 if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1
-    if slack_client.rtm_connect():
-        print("Sacred Deer connected and running!")
-        while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
-    else:
-        print("Connection failed. Invalid Slack token or bot ID?")
+    bot = Bot('sacred_deer', os.environ.get('TOKEN'))
+    bot.run()
---
+++ sacreddeer/tests/test_bot.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest import mock
+
+from sacreddeer import bot
+
+
+class TestBot(unittest.TestCase):
+    @mock.patch('slackclient.SlackClient.api_call')
+    def _init_test_bot(self, mock_api_call):
+        mock_api_call.return_value = {'ok': True,
+                                      'members': [{'name': 'test_deer',
+                                                   'id': '12345'}]}
+        test_bot = bot.Bot('test_deer', 'token')
+        self.assertTrue(mock_api_call.called)
+        return test_bot
+
+    def setUp(self):
+        super(TestBot, self).setUp()
+        self.bot = self._init_test_bot()
+
+    @mock.patch('slackclient.SlackClient.api_call')
+    def _handle_command(self, cmd, responses, mock_api_call):
+        self.bot.handle_command(cmd, 'channel')
+        self.assertTrue(mock_api_call.called)
+        call_args = mock_api_call.call_args[0]
+        call_kwargs = mock_api_call.call_args[1]
+        self.assertEqual('chat.postMessage', call_args[0])
+        self.assertTrue(call_kwargs['as_user'])
+        self.assertEqual('channel', call_kwargs['channel'])
+        self.assertIn(call_kwargs['text'], responses)
+
+    def test_handle_command_ua(self):
+        cmd = u'ua Мене звільнять?'
+        responses_ua = [
+            "Так",
+            "Ні",
+            "Немає значення",
+            "Не сци, козаче",
+            "Товсто",
+            "Так, хоча даремно",
+            "Ніколи",
+            "100%",
+            "1 із 100",
+            "Спробуй ще"
+        ]
+        self._handle_command(cmd, responses_ua)
