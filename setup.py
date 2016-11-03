b24d7c9caefaec47984cf2efb601e31b09e15e2c
pannkotsky <kovvalole@gmail.com>
2016-11-03 13:18:18 +0200

    Add Brutal Elk

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setuptools.setup(
     name='sacreddeer',
-    version='1.0',
+    version='1.1',
     description='Ask your stupid questions and Deer will answer',
     url='http://github.com/pannkotsky/sacreddeer',
     author='Valerii Kovalchuk',
@@ -17,6 +17,6 @@ setuptools.setup(
     install_requires=[
         'slackclient',
     ],
-    scripts=['cmd/deer'],
+    scripts=['cmd/deer', 'cmd/elk'],
     zip_safe=False
 )
